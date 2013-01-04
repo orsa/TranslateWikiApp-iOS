58a6c711ebdf9675a980c79aad1f285624fd2c37
Or Sagi <orsagi@gmail.com>
2013-01-04 09:25:38 +0200

    store credentials

--- TranslateWikiApp/ViewController.m
+++ TranslateWikiApp/ViewController.m
@@ -6,8 +6,10 @@
 //  Copyright (c) 2012 translatewiki.net. All rights reserved.
 //
 
+#import <Security/Security.h>
 #import "ViewController.h"
 #import "TWapi.h"
+#import "KeychainItemWrapper.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *passwordText;
@@ -17,12 +19,35 @@
 
 @end
 
+
 @implementation ViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    KeychainItemWrapper * loginKC = [[KeychainItemWrapper alloc] initWithIdentifier:@"login" accessGroup:nil];
+    NSString *nameString  =  [loginKC objectForKey:(__bridge id)(kSecAttrAccount)];
+    NSString *passwString = [loginKC objectForKey:(__bridge id)kSecValueData];
+    
+    if(nameString!=@"" && passwString!=@"")
+    {
+        NSString *resultString = [TWapi TWLoginRequestForUser:nameString WithPassword:passwString];
+        if([resultString isEqualToString:@"Success"])
+        {
+            //then we can skip the login screen
+        }
+        else
+        {
+            [loginKC resetKeychainItem];
+        }
+    }
+    
+    
+    NSLog(@"%@\n",[loginKC objectForKey:(__bridge id)(kSecAttrAccount)]);
+    NSLog(@"%@\n", [loginKC objectForKey:(__bridge id)kSecValueData]);
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -40,5 +65,15 @@
     NSString *passwString = self.password;
 
     self.ResultLabel.text = [TWapi TWLoginRequestForUser:nameString WithPassword:passwString];
+    
+    if([ self.ResultLabel.text isEqualToString:@"Success"])
+    {
+        KeychainItemWrapper * loginKC = [[KeychainItemWrapper alloc] initWithIdentifier:@"login" accessGroup:nil];
+        [loginKC setObject:nameString forKey:(__bridge id)kSecAttrAccount];
+        [loginKC setObject:passwString forKey:(__bridge id)kSecValueData];
+        //logged in - move to next screen
+    }
 }
+
+
 @end
