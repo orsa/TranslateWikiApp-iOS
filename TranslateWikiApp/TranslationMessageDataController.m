b12d710107c0b85fdeba3023d1a2ae180fbdb2cb
Or Sagi <orsagi@gmail.com>
2013-06-10 23:20:18 +0300

    pre-release sync
    minor changes

--- TranslateWikiApp/TranslationMessageDataController.m
+++ TranslateWikiApp/TranslationMessageDataController.m
@@ -108,7 +108,6 @@
                 }];
                 
             }
-            //[message setSource:@"klavlav hoi bi di bam bam, klavlav hoi bi di bam bam, klavlav hoi bi di bam bam, klavlav hoi bi di bam bam, klavlav hoi bi di bam bam, klavlav hoi bi di bam bam, klavlav hoi bi di bam bam!!!"];
             [self addTranslationMessageWithMessage:message];
         }
     }
