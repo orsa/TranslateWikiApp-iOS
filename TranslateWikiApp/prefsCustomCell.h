991f68344a61ee4c3d8d18cbe1f6f98100cf2d53
Tomer Tuchner <tomertuchner@walla.co.il>
2013-06-21 16:30:33 +0300

    fixing layout in prefs view - ipad

---
+++ TranslateWikiApp/prefsCustomCell.h
@@ -0,0 +1,13 @@
+//
+//  prefsCustomCell.h
+//  TranslateWikiApp
+//
+//  Created by Tomer Tuchner on 6/21/13.
+//  Copyright (c) 2013 translatewiki.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface prefsCustomCell : UITableViewCell
+
+@end
---
+++ TranslateWikiApp/prefsCustomCell.m
@@ -0,0 +1,47 @@
+//
+//  prefsCustomCell.m
+//  TranslateWikiApp
+//
+//  Created by Tomer Tuchner on 6/21/13.
+//  Copyright (c) 2013 translatewiki.net. All rights reserved.
+//
+
+#import "prefsCustomCell.h"
+
+@implementation prefsCustomCell
+
+-(void)awakeFromNib{
+    [super awakeFromNib];
+    for(NSLayoutConstraint *cellConstraint in self.constraints){
+        [self removeConstraint:cellConstraint];
+        id firstItem = cellConstraint.firstItem == self ? self.contentView : cellConstraint.firstItem;
+        id seccondItem = cellConstraint.secondItem == self ? self.contentView : cellConstraint.secondItem;
+        NSLayoutConstraint* contentViewConstraint =
+        [NSLayoutConstraint constraintWithItem:firstItem
+                                     attribute:cellConstraint.firstAttribute
+                                     relatedBy:cellConstraint.relation
+                                        toItem:seccondItem
+                                     attribute:cellConstraint.secondAttribute
+                                    multiplier:cellConstraint.multiplier
+                                      constant:cellConstraint.constant];
+        [self.contentView addConstraint:contentViewConstraint];
+    }
+}
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
